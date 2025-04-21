faf244b013d10c737622fa14e2a8dd5404442a3e
Adon O <adon.omeri@student.education.wa.edu.au>
2025-04-21 12:30:44 +0800

    Initial Commit

---
+++ SwiftUIVault/SwiftUIVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIVaultApp.swift
+//  SwiftUIVault
+//
+//  Created by Adon Omeri on 21/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
